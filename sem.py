be0c18959458030e5b3df37963a3d70d5ceb5c3e
Momchil Tomov <tomov90@gmail.com>
2017-11-07 17:39:01 -0500

    NICE IT WORKS -- posterior is the same as in Sam's code next up: try some other values; also print it

--- sem.py
+++ sem.py
@@ -7,10 +7,10 @@ class SEM(object):
     # initialize the SEM object
     #
     def __init__(self, opts):
-        self.lambda = opts['lambda']
-        self.initial_x = opts['initial_scene']
+        self.lmda = opts['lambda']
         self.beta = opts['beta']
         self.eta = opts['eta']
+        self.alpha = opts['alpha']
 
         self.reset()
 
@@ -31,38 +31,53 @@ class SEM(object):
     #
     def scene(self, x):
         K = 0
-        if self.e
-            K = np.argmax(self.e) # current # of distinct event types
+        if self.e:
+            K = np.argmax(self.e) + 1 # current # of distinct event types
+
+        print '\n'
+        print 'x = ', x
+        print 'K = ', K
 
         # CRP prior from Eq 4 
         # prior[k] = P(e_n = k | e_1:n-1)
         #
-        if ~self.e: # it's the very first scene
+        print 'e hat = ', self.e
+        if not self.e: # it's the very first scene
             prior = [1] # only 1 possible event type
-            self.model = LDS(len(x), self.eta)
         else:
             prior = [None] * (K + 1)
-            self.C = np.bincount(self.e) # C_k = number of times each event type k occurred
+            C = np.bincount(self.e) # C_k = number of times each event type k occurred
+            print 'C = ', C
             for k in range(K + 1):
                 if k < K: # k is 0-indexed
-                    prior[k] = C[k] + self.lambda * (e[-1] == k) # already seen event type (k <= K)
+                    prior[k] = C[k] + self.lmda * (self.e[-1] == k) # already seen event type (k <= K)
                 else:
                     assert k == K
                     prior[k] = self.alpha # new event type (k == K + 1)
-                    self.model[k] = LDS(len(x), self.eta)
         prior = np.array(prior, dtype=float) / sum(prior) # normalize prior
+
+        print 'prior = ', prior
+
+        while len(self.model) < K + 1: # make sure the new event types have associated models
+            self.model.append(LDS(len(x), self.eta))
 
         # Likelihood from Eq 2 and Eq 7
         # lik[k] = P(s_n = x_s | s_n-1, e_n = e_n-1 = k)       MOMCHIL: note the difference from the equation
         #
-        next_x = [] # next scene x_s' = f(x_s, e = k, theta), for each event type k
+        next_x = [None] * (K + 1) # next scene x_s' = f(x_s, e = k, theta), for each event type k
+        lik = [None] * (K + 1)
         for k in range(K + 1):
-            if ~self.x or self.e[-1] ~= k # it's the very first scene OR the previous event type is different    MOMCHIL: note the difference from Sam's code: it doesn't make sense unless we're resegmenting
+            if not self.x: # or self.e[-1] != k:
+                # it's the very first scene OR the previous event type is different
+                # MOMCHIL: note the difference from Sam's code: it doesn't make sense unless we're resegmenting
+                # MOMCHIL: scratch that; we basically are admitting that the previous scene was segmented incorrectl, however we're accepting this as a loss and just moving along
                 next_x[k] = self.model[k].initial_x()
-            elif:
+            else:
                 next_x[k] = self.model[k].next_x(self.x[-1])
-            lik[k] = multivarite_normal.pdf(x, next_x[k], self.beta * np.eye(len(x))) # Eq 2 TODO replace with log likelihood from Eq 7
+            lik[k] = multivariate_normal.pdf(x, next_x[k], self.beta * np.eye(len(x))) # Eq 2 TODO replace with log likelihood from Eq 7
         lik = np.array(lik, dtype=float)
+
+        print 'lik = ', lik
 
         # Posterior from Eq 5
         # post[k] = P(e_n = k | s_1:n)
@@ -71,10 +86,14 @@ class SEM(object):
         post = lik * prior;
         post = post / np.sum(post) # normalize posterior
 
-        self.model[k].update(self.x[-1], x) # update event model
+        print 'post = ', post
 
-        # update event type MAPs
+        # MAP of event type
         self.e.append(np.argmax(post))
+
+        # update event model of MAP event type
+        prev_x = self.x[-1] if self.x else self.model[self.e[-1]].initial_x() # if no previous scene, use default initial scene
+        self.model[self.e[-1]].update(prev_x, x)
 
         # update histories
         self.lik.append(lik)
@@ -83,8 +102,11 @@ class SEM(object):
         self.x.append(x)
         self.next_x.append(next_x)
 
+        return self.e[-1], post
+
     def segment(self, X):
         self.reset()
         for x in X:
+            print x
             self.scene(x)
-        return self.e
+        return self.e, self.post
